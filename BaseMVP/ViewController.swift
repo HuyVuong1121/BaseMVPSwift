a90961c279da107d1091862909c1ea93817ca4a2
ChungTV <chungtv@topica.edu.vn>
2019-08-16 09:48:30 +0700

    Initial Commit

---
+++ BaseMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseMVP
+//
+//  Created by Chung-Sama on 8/16/19.
+//  Copyright © 2019 ChungTV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
